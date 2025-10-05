6869c0b78dbb83abbb2b634c88a29354c5c2406f
Jen-Bati <jain_aana@hotmail.com>
2025-10-05 14:24:02 +0545

    updated day2 live video

--- js/video.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("unmuteBtn").addEventListener("click", function () {
-        const iframe = document.getElementById("liveFrame");
-        const src = iframe.src.replace("mute=1", "mute=0");
-        iframe.src = src;
-        this.style.display = "none"; // hide button after unmuting
-      });
